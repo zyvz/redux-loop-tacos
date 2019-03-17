3b695839cb27975028da12477b059cb5a3bd18d2
baruchlane <baruchlane@users.noreply.github.com>
2019-03-17 12:31:36 -0400

    with redux-loop side effects

--- src/redux.js
+++ src/redux.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import { install } from 'redux-loop';
 import reducer, { initialState } from './reducer';
 
-export const store = createStore(reducer, initialState);
+export const store = createStore(reducer, initialState, install());
